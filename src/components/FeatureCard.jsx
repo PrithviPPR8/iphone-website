5a35a4e7552121e765b3a78b6354d4e6ed58955e
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-08-22 10:26:47 +0530

    Basic design of FeatureCard done. Mapped its properties

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { features } from "../../constants"
 
-const FeatureCard = () => {
+const FeatureCard = ({ imgUrl, title, description }) => {
   return (
-    <div>
-        <img src={features[0].imgUrl} />
-        <h3>{features[0].title}</h3>
-        <h2>{features[0].description}</h2>
+    <div className="relative">
+        <img src={imgUrl} className="rounded-3xl" />
+        <div className="absolute top-7 left-7">
+          <h3 className="text-white text-2xl font-medium">{title}</h3>
+          <h2 className="text-white text-3xl font-semibold mt-1">{description}</h2>
+        </div>
     </div>
   )
 }
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import FeatureCard from "./FeatureCard"
+import { features } from "../../constants"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 const Features = () => {
   return (
     <div className="h-screen">
       <h1 className="text-5xl font-semibold">Get to know iPhone</h1>
-      <FeatureCard />
+      {/* 33% of the carousel width. */}
+      <Carousel>
+        <CarouselContent>
+          {features.map((feature) => (
+            <CarouselItem className="basis-1/3">
+              <FeatureCard 
+                imgUrl = {feature.imgUrl}  
+                title = {feature.title}
+                description = {feature.description}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   )
 }
